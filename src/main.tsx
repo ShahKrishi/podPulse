636087d267905012e167df3fca2ca155b5dda87a
Krishi <krishiiishah15@gmail.com>
2025-09-02 21:12:25 +0530

    Did more modification

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './routes/Routes';
 import { Provider } from 'react-redux';
 import { store } from './utils/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
